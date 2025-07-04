403063ec902b2a9a61c087f6249495e02ed6fafa
kvgo1 <kavya78388@gmail.com>
2025-07-04 22:56:11 +0530

    Animation

--- exam-front/src/app/pages/signup/signup.component.ts
+++ exam-front/src/app/pages/signup/signup.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [], // Add CommonModule, FormsModule, etc. if needed
+  imports: [MatFormFieldModule,MatInputModule,MatButtonModule], // Add CommonModule, FormsModule, etc. if needed
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SignupComponent {}
